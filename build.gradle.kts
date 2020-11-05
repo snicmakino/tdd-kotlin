d8254e2ea63b9f1b142a5ef1ef8184f941e16895
snicmakino <snicmakino@gmail.com>
2020-11-06 06:19:29 +0900

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,19 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.4.10"
+    application
+}
+group = "me.shinichi"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+tasks.withType<KotlinCompile>() {
+    kotlinOptions.jvmTarget = "13"
+}
+application {
+    mainClassName = "MainKt"
+}
