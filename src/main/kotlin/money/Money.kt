0da8eae5e8deca5a4bb3c1a434f96deafde20094
snicmakino <snicmakino@gmail.com>
2020-11-08 04:36:21 +0900

    1-8 factory methodを追加する
    Dollarクラスを直接扱わないように実装を隠すという作業を行った。
    サブクラスを直接利用しないようにするという意識で継承を扱ったことは無かったのでこれは良さそう。
    インタフェースで実装する事が多いが、実際にコードが同じ意味で重複する場合は、継承を使うのはありかもしれない

--- src/main/kotlin/money/Money.kt
+++ src/main/kotlin/money/Money.kt
@@ -1,11 +1,19 @@
 package money
 
-open class Money(protected val amount: Int) {
+abstract class Money(protected val amount: Int) {
     override fun equals(other: Any?): Boolean {
         if (other !is Money) {
             return false
         }
         return amount == other.amount
                 && this.javaClass.kotlin == other.javaClass.kotlin
+    }
+
+    abstract fun times(multiplier: Int): Money
+
+    companion object {
+        fun dollar(amount: Int): Money {
+            return Dollar(amount)
+        }
     }
 }
--- src/test/kotlin/MoneyTest.kt
+++ src/test/kotlin/MoneyTest.kt
@@ -1,22 +1,22 @@
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.shouldNotBe
-import money.Dollar
 import money.Franc
+import money.Money
 
 class MoneyTest : FunSpec({
     test("掛け算をテストする") {
-        val five = Dollar(5)
-        five.times(2) shouldBe Dollar(10)
-        five.times(3) shouldBe Dollar(15)
+        val five = Money.dollar(5)
+        five.times(2) shouldBe Money.dollar(10)
+        five.times(3) shouldBe Money.dollar(15)
     }
 
     test("等価性をテストする") {
-        Dollar(5) shouldBe Dollar(5)
-        Dollar(5) shouldNotBe Dollar(6)
+        Money.dollar(5) shouldBe Money.dollar(5)
+        Money.dollar(5) shouldNotBe Money.dollar(6)
         Franc(5) shouldBe Franc(5)
         Franc(5) shouldNotBe Franc(6)
-        Dollar(5) shouldNotBe Franc(5)
+        Money.dollar(5) shouldNotBe Franc(5)
     }
 
     test("フランの掛け算をテストする") {
