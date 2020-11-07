2ea4816217cd5c32acd73089896d0acfdf6672d6
snicmakino <snicmakino@gmail.com>
2020-11-08 03:32:45 +0900

    1-3 equalsの実装
    三角測量にて実装した

--- src/main/kotlin/Dollar.kt
+++ src/main/kotlin/Dollar.kt
@@ -3,4 +3,11 @@ class Dollar(val amount: Int) {
     fun times(multiplier: Int): Dollar {
         return Dollar(amount * multiplier)
     }
+
+    override fun equals(other: Any?): Boolean {
+        if (other !is Dollar) {
+            return false
+        }
+        return amount == other.amount
+    }
 }
--- src/test/kotlin/MoneyTest.kt
+++ src/test/kotlin/MoneyTest.kt
@@ -1,5 +1,6 @@
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
+import io.kotest.matchers.shouldNotBe
 
 class MoneyTest : FunSpec({
     test("掛け算をテストする") {
@@ -8,5 +9,10 @@ class MoneyTest : FunSpec({
         product.amount shouldBe 10
         product = five.times(3)
         product.amount shouldBe 15
+    }
+
+    test("等価性をテストする") {
+        Dollar(5) shouldBe Dollar(5)
+        Dollar(5) shouldNotBe Dollar(6)
     }
 })
