24828181e29aff1ceceaec7d77531759d184f466
snicmakino <snicmakino@gmail.com>
2020-11-10 04:35:21 +0900

    2-19 setUpを最初に呼び指す実装の完了
    テスティングフレームワークを作ったことがないので恐る恐る進めている。
    完成したタイミングで、見覚えのある動作になっていて驚いている。
    テストケースから、このコードは導出できない気もするが、何度か読み返してなじませていきたい

--- src/main/xunit/xunit.kt
+++ src/main/xunit/xunit.kt
@@ -1,9 +1,12 @@
 fun main(args: Array<String>) {
     TestCaseTest("testRunning").run()
+    TestCaseTest("testSetUp").run()
 }
 
 open class TestCase(private val name: String) {
+    open fun setUp() {}
     fun run() {
+        setUp()
         val jClass = this.javaClass
         val method = jClass.getMethod(name)
         method.invoke(this)
@@ -11,17 +14,33 @@ open class TestCase(private val name: String) {
 }
 
 class WasRun(name: String) : TestCase(name) {
-    var wasRun: Boolean = false
+    var wasSetUp = false
+    var wasRun = false
+
+    override fun setUp() {
+        wasRun = false
+        wasSetUp = true
+    }
+
     fun testMethod() {
         wasRun = true
     }
 }
 
 class TestCaseTest(name: String) : TestCase(name) {
+    lateinit var test: WasRun
+
+    override fun setUp() {
+        test = WasRun("testMethod")
+    }
+
     fun testRunning() {
-        val test = WasRun("testMethod")
-        check(!test.wasRun)
         test.run()
         check(test.wasRun)
+    }
+
+    fun testSetUp() {
+        test.run()
+        check(test.wasSetUp)
     }
 }
