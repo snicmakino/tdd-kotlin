a74d0d1b155fae589f6460da06af516fa4e7b0c0
snicmakino <snicmakino@gmail.com>
2020-11-11 03:46:34 +0900

    2-21 testBrokenMethodを実装しようとしたタイミングで考慮することが多かった
    一旦テストをコメントアウトして、もう少し細かいテストをする必要がある。
    覚えて置かなければいけない事象がある場合はこのようにしたほうが良さそう

--- src/main/xunit/xunit.kt
+++ src/main/xunit/xunit.kt
@@ -1,16 +1,23 @@
 fun main(args: Array<String>) {
     TestCaseTest("testTemplateMethod").run()
+    TestCaseTest("testResult").run()
+//    TestCaseTest("testBrokenMethod").run()
 }
 
 open class TestCase(private val name: String) {
+
     open fun setUp() {}
     open fun tearDown() {}
-    fun run() {
+
+    fun run(): TestResult {
+        val result = TestResult()
+        result.testStarted()
         setUp()
         val jClass = this.javaClass
         val method = jClass.getMethod(name)
         method.invoke(this)
         tearDown()
+        return result
     }
 }
 
@@ -25,8 +32,24 @@ class WasRun(name: String) : TestCase(name) {
         log += "testMethod "
     }
 
+    fun testBrokenMethod() {
+        throw Exception()
+    }
+
     override fun tearDown() {
         log += "tearDown "
+    }
+}
+
+class TestResult {
+    var runCount = 0
+
+    fun testStarted() {
+        runCount++
+    }
+
+    fun summary(): String {
+        return "$runCount run, 0 failed"
     }
 }
 
@@ -41,5 +64,11 @@ class TestCaseTest(name: String) : TestCase(name) {
         val test = WasRun("testMethod")
         val result: TestResult = test.run()
         check("1 run, 0 failed" == result.summary())
+    }
+
+    fun testFailedResult() {
+        val test = WasRun("testBrokenMethod")
+        val result: TestResult = test.run()
+        check("1 run, 1 failed" == result.summary())
     }
 }
